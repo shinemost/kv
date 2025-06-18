b4e2a04f7fd9db854559f39b58eb614c74f43bfb
shinemost <supertain147@163.com>
2025-06-19 00:10:14 +0800

    kv server 练习一

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod pb;
 mod storage;
 mod error;
+mod service;
 
 pub use pb::abi::*;
 pub use storage::*;
 pub use error::KvError;
+pub use service::*;
 
 
 
--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,6 +1,8 @@
 pub mod abi;
 
+use http::StatusCode;
 use abi::{command_request::RequestData, *};
+use crate::KvError;
 
 impl CommandRequest {
     /// 创建 HSET 命令
@@ -10,6 +12,18 @@ impl CommandRequest {
                 table: table.into(),
                 pair: Some(Kvpair::new(key, value)),
             })),
+        }
+    }
+
+    pub fn new_hget(table: impl Into<String>, key: impl Into<String>) -> Self {
+        Self{
+            request_data: Some(RequestData::Hget(Hget {table: table.into(), key: key.into()})),
+        }
+    }
+
+    pub fn new_hgetall(table: impl Into<String>) -> Self {
+        Self{
+            request_data: Some(RequestData::Hgetall(Hgetall{table: table.into()})),
         }
     }
 }
@@ -39,5 +53,55 @@ impl From<&str> for Value {
         Self {
             value: Some(value::Value::String(s.into())),
         }
+    }
+}
+
+impl From<i64> for Value {
+    fn from(i: i64) -> Self {
+        Self{
+            value: Some(value::Value::Integer(i)),
+        }
+    }
+}
+
+/// 从 Value 转换成 CommandResponse
+impl From<Value> for CommandResponse {
+    fn from(v: Value) -> Self {
+        Self {
+            status: StatusCode::OK.as_u16() as _,
+            values: vec![v],
+            ..Default::default()
+        }
+    }
+}
+
+/// 从 Vec<Kvpair> 转换成 CommandResponse
+impl From<Vec<Kvpair>> for CommandResponse {
+    fn from(v: Vec<Kvpair>) -> Self {
+        Self {
+            status: StatusCode::OK.as_u16() as _,
+            pairs: v,
+            ..Default::default()
+        }
+    }
+}
+
+/// 从 KvError 转换成 CommandResponse
+impl From<KvError> for CommandResponse {
+    fn from(e: KvError) -> Self {
+        let mut result = Self {
+            status: StatusCode::INTERNAL_SERVER_ERROR.as_u16() as _,
+            message: e.to_string(),
+            values: vec![],
+            pairs: vec![],
+        };
+
+        match e {
+            KvError::NotFound(_, _) => result.status = StatusCode::NOT_FOUND.as_u16() as _,
+            KvError::InvalidCommand(_) => result.status = StatusCode::BAD_REQUEST.as_u16() as _,
+            _ => {}
+        }
+
+        result
     }
 }

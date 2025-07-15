d094eee54317a5041ad1e1ae42a45115909e392c
shinemost <supertain147@163.com>
2025-07-15 23:39:12 +0800

    事件通知执行代码编写

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -10,12 +10,20 @@ pub trait CommandService {
 pub struct Service {
     // inner: Arc<ServiceInner<Store>>,
     pub store: Arc<dyn Storage>,
+    on_received: Vec<fn(&CommandRequest)>,
+    on_executed: Vec<fn(&CommandResponse)>,
+    on_before_send: Vec<fn(&mut CommandResponse)>,
+    on_after_send: Vec<fn()>,
 }
 
 impl Clone for Service {
     fn clone(&self) -> Self {
         Self {
             store: Arc::clone(&self.store),
+            on_received: self.on_received.clone(),
+            on_executed: self.on_executed.clone(),
+            on_before_send: self.on_before_send.clone(),
+            on_after_send: self.on_after_send.clone(),
         }
     }
 }
@@ -29,19 +37,48 @@ impl Service {
     pub fn new<S:Storage>(store: S) -> Self {
         Self {
             store: Arc::new(store),
+            on_received: Vec::new(),
+            on_executed: Vec::new(),
+            on_before_send: Vec::new(),
+            on_after_send: Vec::new(),
         }
     }
 
     pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
         debug!("Got request: {:?}", cmd);
-        // TODO: 发送 on_received 事件
+        self.on_received.notify(&cmd);
         // self.store.deref()解引用 Arc<dyn Storage> -> &dyn Storage
         // self.store.as_ref()同理
-        let res = dispatch(cmd, self.store.as_ref());
+        let mut res = dispatch(cmd, self.store.as_ref());
         debug!("Executed response: {:?}", res);
-        // TODO: 发送 on_executed 事件
+
+        self.on_executed.notify(&res);
+        self.on_before_send.notify(&mut res);
+        if !self.on_before_send.is_empty() {
+            debug!("Modified response: {:?}", res);
+        }
 
         res
+    }
+
+    pub fn fn_received(mut self, f: fn(&CommandRequest)) -> Self {
+        self.on_received.push(f);
+        self
+    }
+
+    pub fn fn_executed(mut self, f: fn(&CommandResponse)) -> Self {
+        self.on_executed.push(f);
+        self
+    }
+
+    pub fn fn_before_send(mut self, f: fn(&mut CommandResponse)) -> Self {
+        self.on_before_send.push(f);
+        self
+    }
+
+    pub fn fn_after_send(mut self, f: fn()) -> Self {
+        self.on_after_send.push(f);
+        self
     }
 }
 
@@ -56,11 +93,42 @@ pub fn dispatch(cmd: CommandRequest, store: &dyn Storage) -> CommandResponse {
     }
 }
 
+/// 事件通知（不可变事件）
+pub trait Notify<Arg> {
+    fn notify(&self, arg: &Arg);
+}
+
+/// 事件通知（可变事件）
+pub trait NotifyMut<Arg> {
+    fn notify(&self, arg: &mut Arg);
+}
+
+impl<Arg> Notify<Arg> for Vec<fn(&Arg)> {
+    #[inline]
+    fn notify(&self, arg: &Arg) {
+        for f in self {
+            f(arg)
+        }
+    }
+}
+
+impl<Arg> NotifyMut<Arg> for Vec<fn(&mut Arg)> {
+    #[inline]
+    fn notify(&self, arg: &mut Arg) {
+        for f in self {
+            f(arg)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use
+    super::*;
     use crate::{MemTable, Value};
     use std::thread;
+    use http::StatusCode;
+    use tracing::info;
 
     #[test]
     fn service_should_works() {
@@ -80,6 +148,34 @@ mod tests {
         // 在当前线程下读取 table t1 的 k1，应该返回 v1
         let res = service.execute(CommandRequest::new_hget("t1", "k1"));
         assert_res_ok(res, &["v1".into()], &[]);
+    }
+
+    #[test]
+    fn event_registration_should_work() {
+        fn b(cmd: &CommandRequest) {
+            info!("Got {:?}", cmd);
+        }
+        fn c(res: &CommandResponse) {
+            info!("{:?}", res);
+        }
+        fn d(res: &mut CommandResponse) {
+            res.status = StatusCode::CREATED.as_u16() as _;
+        }
+        fn e() {
+            info!("Data is sent");
+        }
+
+        let service: Service = Service::new(MemTable::default())
+            .fn_received(|_: &CommandRequest| {})
+            .fn_received(b)
+            .fn_executed(c)
+            .fn_before_send(d)
+            .fn_after_send(e);
+
+        let res = service.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+        assert_eq!(res.status, StatusCode::CREATED.as_u16() as _);
+        assert_eq!(res.message, "");
+        assert_eq!(res.values, vec![Value::default()]);
     }
 }
 
@@ -105,3 +201,5 @@ pub fn assert_res_error(res: CommandResponse, code: u32, msg: &str) {
     assert_eq!(res.values, &[]);
     assert_eq!(res.pairs, &[]);
 }
+
+

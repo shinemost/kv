9d88d821e3ff2d1db0131decafa2a2f595f098f9
shinemost <supertain147@163.com>
2025-07-22 22:49:41 +0800

    使用 sleddb 三方库完成kv 持久化

--- src/storage/memory.rs
+++ src/storage/memory.rs
@@ -1,4 +1,4 @@
-use crate::{KvError, Kvpair, Storage, Value};
+use crate::{KvError, Kvpair, Storage, StorageIter, Value};
 use dashmap::{mapref::one::Ref, DashMap};
 
 /// 使用 DashMap 构建的 MemTable，实现了 Storage trait
@@ -67,23 +67,4 @@ impl From<(String, Value)> for Kvpair {
     }
 }
 
-pub struct StorageIter<T> {
-    data: T,
-}
 
-impl<T> StorageIter<T> {
-    pub fn new(data: T) -> Self {
-        Self { data }
-    }
-}
-
-impl<T> Iterator for StorageIter<T>
-where
-    T: Iterator,
-    T::Item: Into<Kvpair>,
-{
-    type Item = Kvpair;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.data.next().map(|v| v.into())
-    }
-}
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,4 +1,7 @@
 mod memory;
+mod sleddb;
+
+pub use sleddb::SledDb;
 pub use memory::MemTable;
 
 use crate::{KvError, Kvpair, Value};
@@ -18,9 +21,31 @@ pub trait Storage: Send + Sync+'static {
     fn get_iter(&self, table: &str) -> Result<Box<dyn Iterator<Item = Kvpair>>, KvError>;
 }
 
+pub struct StorageIter<T> {
+    data: T,
+}
+
+impl<T> StorageIter<T> {
+    pub fn new(data: T) -> Self {
+        Self { data }
+    }
+}
+
+impl<T> Iterator for StorageIter<T>
+where
+    T: Iterator,
+    T::Item: Into<Kvpair>,
+{
+    type Item = Kvpair;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.data.next().map(|v| v.into())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempfile::tempdir;
 
     #[test]
     fn memtable_basic_interface_should_work() {
@@ -97,5 +122,26 @@ mod tests {
                 Kvpair::new("k2", "v2".into())
             ]
         )
+    }
+
+    #[test]
+    fn sleddb_basic_interface_should_work() {
+        let dir = tempdir().unwrap();
+        let store = SledDb::new(dir);
+        test_basi_interface(store);
+    }
+
+    #[test]
+    fn sleddb_get_all_should_work() {
+        let dir = tempdir().unwrap();
+        let store = SledDb::new(dir);
+        test_get_all(store);
+    }
+
+    #[test]
+    fn sleddb_iter_should_work() {
+        let dir = tempdir().unwrap();
+        let store = SledDb::new(dir);
+        test_get_iter(store);
     }
 }
